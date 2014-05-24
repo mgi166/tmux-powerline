b256c755bebf78a8ea8242e4c2cc9475658431e7
mgi166 <skskoari@gmail.com>
2014-05-24 20:28:08 +0900

    * Add new function "get_tmux_cwd" because it is wrong display ruby version in $PWD

--- segments/ruby-version.sh
+++ segments/ruby-version.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+# Source lib to get the function get_tmux_pwd
+source "${TMUX_POWERLINE_DIR_LIB}/tmux_adapter.sh"
+
 symbol="ruby: "
 
 run_segment() {
+    tmux_path=$(get_tmux_cwd)
+    cd "$tmux_path"
+
     if rbenv >/dev/null 2>&1; then
         version=$(ruby -v | awk -F ' ' '{print $2}')
         echo "${symbol}${version}"
